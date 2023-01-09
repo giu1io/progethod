3bae45b8cee87ba3b010f7daa06214d2c35fcba6
Giulio Montagner <me@giuliomontagner.com>
2023-01-09 19:36:12 +0200

    fix: ignore archived projects

--- utils/updateApiData.js
+++ utils/updateApiData.js
@@ -9,14 +9,16 @@ export async function updateApiData ($axios, store) {
         date: format(new Date(), 'yyyy-MM-dd')
       }
     })
-    const projects = data.projects.map(({ project, areas }) => ({
-      id: project.id,
-      name: project.name,
-      isAutomatic: project.project_type.is_timesheet_automatic,
-      areas: areas
-        .filter(({ on }) => on)
-        .map(({ id, name }) => ({ id, name }))
-    }))
+    const projects = data.projects
+      .filter(({ project }) => project.archived !== true)
+      .map(({ project, areas }) => ({
+        id: project.id,
+        name: project.name,
+        isAutomatic: project.project_type.is_timesheet_automatic,
+        areas: areas
+          .filter(({ on }) => on)
+          .map(({ id, name }) => ({ id, name }))
+      }))
 
     store.commit('apiData/replace', projects)
   } catch (error) {
